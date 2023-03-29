ff5f388707e73e7cb80960da562acf50f63b8f62
Oleksii_Baiev <bayevaleksey@gmail.com>
2023-03-29 14:11:19 +0300

    hw12

---
+++ HW12/dumpHW120304.sql
@@ -0,0 +1,97 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.2
+-- Dumped by pg_dump version 15.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: adminpack; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS adminpack WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION adminpack; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION adminpack IS 'administrative functions for PostgreSQL';
+
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: word; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.word (
+    id integer NOT NULL,
+    name character varying(255),
+    info text
+);
+
+
+ALTER TABLE public.word OWNER TO postgres;
+
+--
+-- Name: word_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.word_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.word_id_seq OWNER TO postgres;
+
+--
+-- Name: word_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.word_id_seq OWNED BY public.word.id;
+
+
+--
+-- Name: word id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.word ALTER COLUMN id SET DEFAULT nextval('public.word_id_seq'::regclass);
+
+
+--
+-- Data for Name: word; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.word (id, name, info) FROM stdin;
+\.
+
+
+--
+-- Name: word_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.word_id_seq', 2, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
